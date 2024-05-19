14740b6f40150f045eabe007843cee4edfaace53
Percy.Ku.Dev1 <percyku19@gmail.com>
2024-05-19 16:32:12 +0800

    delete AppDao AppDaoImpl AppDaoRepository

--- src/main/java/com/percyku/learningserver/learningserverspringboot/dao/imp/AppDaoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.percyku.learningserver.learningserverspringboot.dao.imp;
-
-import com.percyku.learningserver.learningserverspringboot.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppDaoRepository extends JpaRepository<User,Long> {
-
-
-}

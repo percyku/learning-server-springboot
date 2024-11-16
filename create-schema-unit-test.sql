c4791ef7d9396bcdfff3966d2882c73c0755f3ad
Percy.Ku.Dev1 <percyku19@gmail.com>
2024-11-16 23:53:11 +0800

    update README

---
+++ create-schema-unit-test.sql
@@ -0,0 +1,7 @@
+DROP SCHEMA IF EXISTS `learningsys_unit_test`;
+
+CREATE SCHEMA `learningsys_unit_test`;
+
+use `learningsys_unit_test`;
+
+SET FOREIGN_KEY_CHECKS = 0;
--- create-tables-unit-test.sql
+++ create-tables-unit-test.sql
@@ -1,4 +1,4 @@
-use `learningsys`;
+use `learningsys_unit_test`;
 --
 -- Table structure for table `user`
 --
@@ -132,23 +132,8 @@ SET FOREIGN_KEY_CHECKS = 1;
 
 
 
---
--- Dumping data for table `user`
---
--- NOTE: The passwords are encrypted using BCrypt
---
---
--- Default passwords here are: fun123
---
+use `learningsys_unit_test`;
 
-INSERT INTO `user` (`username`,`password`,`enabled`, `first_name`, `last_name`, `email`)
-VALUES 
-('studnet1','$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K',1,'student1', 'test', 'studnet1@gmail.com'),
-('instructor1','$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K',1,'instructor1', 'test', 'instructor1@gmail.com')
-
-
-
---
 -- Dumping data for table `roles`
 --
 
@@ -157,18 +142,3 @@ VALUES
 ('ROLE_STUDENT'),('ROLE_INSTRUCTOR');
 
 SET FOREIGN_KEY_CHECKS = 0;
-
-
-
-
-
-
-
---
--- Dumping data for table `users_roles`
---
-
-INSERT INTO `users_roles` (user_id,role_id)
-VALUES 
-(1, 1),
-(2, 2),
